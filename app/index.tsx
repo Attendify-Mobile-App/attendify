2ed43aa31d25cd09d61dd17a8d1a2a836b2ea659
Akar <akarsihar@gmail.com>
2026-02-02 23:03:54 +0530

    login issue

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,9 @@
 import { Redirect } from 'expo-router';
+import { LOGIN_SCREEN, SELECT_CLASS_SCREEN } from '@/constants/navigation/path';
+import { useAppSelector } from '@/store/hooks';
 
 export default function Index() {
-  return <Redirect href="/screens/auth/login/login" />;
+  const accessToken = useAppSelector((state) => state.auth.accessToken);
+
+  return <Redirect href={accessToken ? SELECT_CLASS_SCREEN : LOGIN_SCREEN} />;
 }
