669c636824797cccf3854ec99af4226d57ace879
Akar <akarsihar@gmail.com>
2026-02-11 06:36:57 +0530

    apk preview

--- constants/api.ts
+++ constants/api.ts
@@ -1,2 +1,6 @@
 export const API_BASE_URL =
   process.env.EXPO_PUBLIC_API_BASE_URL ?? 'http://10.120.165.27:3000';
+
+
+// export const API_BASE_URL =
+//   process.env.EXPO_PUBLIC_API_BASE_URL ?? 'https://attendify-be-production.up.railway.app/';
